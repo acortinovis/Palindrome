5354d84ef434149511c8511b8735af8d15d51c52
acortinovis <adelio.cortinovis@gmail.com>
2025-02-06 12:38:37 -0800

    started palindrome
    started palindrome program

---
+++ main.py
@@ -0,0 +1,12 @@
+# TASK: Write a program that checks if a word is a palindrome.
+
+# Prompt the user to enter a string  
+word=(input("enter the word to check if it's a palindrome ")).strip()
+
+# Reverse the string by slicing 
+word2 = word[::-1]
+
+# Check if the original string is equal to the reversed string  
+# If they are equal, it's a palindrome  
+# If they are not equal, it's not a palindrome  
+
